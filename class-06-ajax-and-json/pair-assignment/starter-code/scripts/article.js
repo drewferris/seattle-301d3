098a3204f23a9f8240fa7b590dab254ff7f427bd
Dustin McBride <dustinbm@gmail.com>
2016-04-18 14:39:13 -0700

    strech goal completed

--- class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
+++ class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
@@ -1,10 +1,10 @@
-function Article (opts) {
-  this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
-  this.title = opts.title;
-  this.category = opts.category;
-  this.body = opts.body;
-  this.publishedOn = opts.publishedOn;
+function Article(opts) {
+    this.author = opts.author;
+    this.authorUrl = opts.authorUrl;
+    this.title = opts.title;
+    this.category = opts.category;
+    this.body = opts.body;
+    this.publishedOn = opts.publishedOn;
 }
 
 // DONE: Instead of a global `articles = []` array, let's track this list of all articles directly on the
@@ -13,15 +13,16 @@ function Article (opts) {
 // a key/value pair to track, that relates to ALL of the Article objects, so it does not belong on
 // the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
+var etag = '';
 
 Article.prototype.toHtml = function() {
-  var template = Handlebars.compile($('#article-template').text());
+    var template = Handlebars.compile($('#article-template').text());
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  this.body = marked(this.body);
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.body = marked(this.body);
 
-  return template(this);
+    return template(this);
 };
 
 // DONE: There are some other functions that also relate to articles across the board, rather than
@@ -32,49 +33,74 @@ Article.prototype.toHtml = function() {
 // and use it to instantiate all the articles. This code is moved from elsewhere, and
 // encapsulated in a simply-named function for clarity.
 Article.loadAll = function(dataPassedIn) {
-  dataPassedIn.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
+    dataPassedIn.sort(function(a, b) {
+        return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
 
-  dataPassedIn.forEach(function(ele) {
-    Article.all.push(new Article(ele));
-  });
+    dataPassedIn.forEach(function(ele) {
+        Article.all.push(new Article(ele));
+    });
 };
 
 // This function below will retrieve the data from either a local or remote source,
 // and process it, then hand off control to the View.
 Article.fetchAll = function() {
-  if (localStorage.hackerIpsum) {
-    // When our data is already in localStorage,
-    // we can load it by calling the .loadAll() method,
-    // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(
-      JSON.parse(localStorage.hackerIpsum)
-    //TODO: What do we pass in here to the .loadAll() method? Be careful
-      // when handling different data types between here and localStorage! you have to parse it localStorage.hackerIpsu
+    if (localStorage.hackerIpsum) {
+        $.ajax({
+            type: 'HEAD',
+            url: 'data/ipsumArticles.json',
+            success: function(data, message, xhr) {
+                var etag = (xhr.getResponseHeader('ETag'));
 
-    );
-    articleView.initIndexPage();//(); //TODO: Change this fake method call to the correct
-    // one that will render the index page.
-  } else {
-    $.getJSON('data/ipsumArticles.json', function(data) {
-      console.log(data);
-      Article.loadAll(data);
-      localStorage.setItem('hackerIpsum', JSON.stringify(data));
-      articleView.initIndexPage();
+                if (localStorage.etag != etag) {
+                
+                        Article.jsonfetch();
+                }
+
+            }
+
+        });
+
+
+
+        Article.loadAll(
+            JSON.parse(localStorage.hackerIpsum)
+            //TODO: What do we pass in here to the .loadAll() method? Be careful
+            // when handling different data types between here and localStorage! you have to parse it localStorage.hackerIpsu
+
+        );
+        articleView.initIndexPage(); //(); //TODO: Change this fake method call to the correct
+        // one that will render the index page.
+    } else {
+        Article.jsonfetch();
+
+
+        // TODO: When we don't already have our data, we need to:
+        // 1. Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
+        // .getJSON
+        // 2. Store the resulting JSON data with the .loadAll method,
+
+        // 3. Cache it in localStorage so we can skip the server call next time,
+
+        // 4. And then render the index page (perhaps with an articleView method?).
+
+    }
+};
+
+Article.jsonfetch = function() {
+    $.ajax({
+        type: 'GET',
+        url: 'data/hackeripsum.json',
+        success: function(data, message, xhr) {
+            Article.loadAll(data);
+            localStorage.setItem('hackerIpsum', JSON.stringify(data));
+            articleView.initIndexPage();
+            etag = (xhr.getResponseHeader('ETag'));
+            localStorage.setItem('etag', etag);
+        }
     });
 
-    // TODO: When we don't already have our data, we need to:
-    // 1. Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
-      // .getJSON
-    // 2. Store the resulting JSON data with the .loadAll method,
-
-    // 3. Cache it in localStorage so we can skip the server call next time,
-
-    // 4. And then render the index page (perhaps with an articleView method?).
-
-  }
-};
+}
 
 /* Great work so far! STRETCH GOAL TIME! Cache the eTag located in Headers, to see if it's updated!
   Article.fetchAll = function() {
