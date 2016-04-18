0104731c123f5d73d264babc1ebe84c1cd6053f4
Dustin McBride <dustinbm@gmail.com>
2016-04-18 15:14:29 -0700

    refactored if (localStorage.etag === etag)

--- class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
+++ class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
@@ -51,31 +51,27 @@ Article.fetchAll = function() {
             url: 'data/ipsumArticles.json',
             success: function(data, message, xhr) {
                 var etag = (xhr.getResponseHeader('ETag'));
+                if (localStorage.etag === etag) {
 
-                if (localStorage.etag != etag) {
-                
-                        Article.jsonfetch();
+                    Article.loadAll(JSON.parse(localStorage.hackerIpsum));
+                    //DONE: What do we pass in here to the .loadAll() method? Be careful
+                    // when handling different data types between here and localStorage! you have to parse it localStorage.hackerIpsu
+
+
+                    articleView.initIndexPage(); //(); //DONE: Change this fake method call to the correct
+                    // one that will render the index page.
+                } else {
+                    Article.jsonfetch();
                 }
-
             }
-
         });
 
 
-
-        Article.loadAll(
-            JSON.parse(localStorage.hackerIpsum)
-            //TODO: What do we pass in here to the .loadAll() method? Be careful
-            // when handling different data types between here and localStorage! you have to parse it localStorage.hackerIpsu
-
-        );
-        articleView.initIndexPage(); //(); //TODO: Change this fake method call to the correct
-        // one that will render the index page.
     } else {
+
         Article.jsonfetch();
 
-
-        // TODO: When we don't already have our data, we need to:
+        // DONE: When we don't already have our data, we need to:
         // 1. Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
         // .getJSON
         // 2. Store the resulting JSON data with the .loadAll method,
@@ -100,7 +96,7 @@ Article.jsonfetch = function() {
         }
     });
 
-}
+};
 
 /* Great work so far! STRETCH GOAL TIME! Cache the eTag located in Headers, to see if it's updated!
   Article.fetchAll = function() {
